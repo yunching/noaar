6e45a08bb5a709732efb026f939dc9fde58e2ae8
Yun Ching Lim <yunching.lim@gmail.com>
2018-11-04 21:37:38 -0500

    Adding timeline

--- R/geoms.R
+++ R/geoms.R
@@ -1,7 +1,7 @@
-GeomTimeline <- ggproto(
+GeomTimeline <- ggplot2::ggproto(
   `_class` = "GeomTimeline", `_inherit` = ggplot2::Geom,
   setup_data = function(data, params) {
-    str(data)
+    # str(data)
     if (is.null(data$y)) {
       data$y <- 0 
     }
@@ -21,7 +21,8 @@ GeomTimeline <- ggproto(
   draw_key = draw_key_point,
   draw_panel = function(data, panel_scales, coord, na.rm = FALSE) {
     coords <- coord$transform(data, panel_scales)
-     str(coords)
+    # str(data)
+    # str(coords)
     # Resize point size to something reasonable
     coords$size <-coords$size / max(coords$size) * 1
     dates_grob <- grid::pointsGrob(
@@ -62,22 +63,88 @@ geom_timeline <-
       inherit.aes = inherit.aes,
       params = list(na.rm = na.rm, ...)
     )
+}
+
+GeomTimelineLabel <- ggplot2::ggproto(
+  `_class` = "GeomTimelineLabel", 
+  `_inherit` = ggplot2::Geom, 
+  required_aes = c("x", "label"),
+  default_aes = ggplot2::aes(n_max = NA), 
+  # draw_key = draw_key_point,
+  setup_data = function(data, params) {
+    if (is.numeric(data$n_max[1])) {
+      data %>% 
+        group_by("group") %>% 
+        top_n(n, size)
+    } else {
+      data
+    }
+  },
+  
+  draw_panel = function(data, panel_scales, coord, na.rm = FALSE) {
+    coords <- coord$transform(data, panel_scales)
+    
+    segments_grob <- grid::segmentsGrob(
+        x0 = coords$x,
+        y0 = coords$y,
+        x1 = coords$x,
+        y1 = coords$y + 0.1,
+        gp = grid::gpar()
+      )
+    
+      labels_grob <- grid::textGrob(
+        x = coords$x,
+        y = coords$y + 0.1,
+        label = coords$label,
+        rot = 45,
+        hjust = -0.1,
+        vjust = -0.1,
+        gp = grid::gpar()
+      )
+    
+    grid::gList(segments_grob, labels_grob)
+  }
+)
+    
+geom_timelinelabel <-
+  function(mapping = NULL,
+           data = NULL,
+           stat = "identity",
+           position = "identity",
+           na.rm = FALSE,
+           show.legend = NA,
+           inherit.aes = TRUE,
+           ...) {
+    ggplot2::layer(
+      geom = GeomTimelineLabel,
+      mapping = mapping,
+      data = data,
+      stat = stat,
+      position = position,
+      show.legend = show.legend,
+      inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    )
   }
 
 cleaned_data %>%
-  filter(country %in% c("CHINA", "MEXICO")) %>% 
+  # filter(country %in% c("CHINA", "MEXICO")) %>% 
   filter(date >= lubridate::ymd("2000-01-01")) %>%
   filter(date < lubridate::ymd("2017-01-01")) %>%
-  # filter(!is.na(intensity)) %>%
-  # filter(!is.na(deaths)) %>%
-  ggplot(mapping = aes(x = date, y = country, size = intensity, color = deaths)) +
-  geom_timeline() + theme(
+  # sample_n(10) %>% 
+  filter(!is.na(intensity)) %>%
+  filter(!is.na(deaths)) %>%
+  ggplot(mapping = aes(x = date)) +
+  geom_timeline(aes(size = intensity, color = deaths)) + theme(
     legend.position = "bottom",
     panel.grid.major.x = element_blank(),
     panel.grid.minor.x = element_blank(),
     panel.grid.major.y = element_blank(),
     panel.grid.minor.y = element_blank(),
-    panel.background =element_blank(),
+    panel.background = element_blank(),
     axis.ticks.x = element_line(),
-    axis.line.x = element_line()
-  )
+    axis.line.x = element_line(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank()
+  ) + geom_timelinelabel(aes(label = location_name))
+
